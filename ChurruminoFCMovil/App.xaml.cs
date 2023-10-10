9b926ebb51c3fc2764700d323a49147d8df0302f
USUARIO <USUARIO@DESKTOP-FOV8LK6>
2023-10-10 07:41:30 -0500

    Add project files.

---
+++ ChurruminoFCMovil/App.xaml.cs
@@ -0,0 +1,29 @@
+﻿using ChurruminoFCMovil.VIEWS;
+using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace ChurruminoFCMovil
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new Inspecciones();
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
---
+++ ChurruminoFCMovil/VIEWS/Inspecciones.xaml.cs
@@ -0,0 +1,14 @@
+﻿using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace ChurruminoFCMovil.VIEWS
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Inspecciones : ContentPage
+    {
+        public Inspecciones()
+        {
+            InitializeComponent();
+        }
+    }
+}
